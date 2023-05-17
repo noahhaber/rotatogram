88f75e2819cbf1887a1bd955e253db6456423109
noahhaber <noahhaber@gmail.com>
2023-05-17 09:22:35 -0400

    feature update
    added bootstrapped errors, small tweak to the way the axis rotation works for readability, and added code to ensure the labels were right side up

--- R/rotatogram.R
+++ R/rotatogram.R
@@ -6,6 +6,8 @@
 #' @param x2 (required) A vector containing the data for the x2 variable
 #' @param x1.label (optional) Changes the x1 axis label to the specified name
 #' @param x2.label (optional) Changes the x2 axis label to the specified name
+#' @param error.bootstrap (optional) Generates and displays bootstrapped errors to enable confidence bands, standard errors, and confidence intervals
+#' @param error.bootstrap.iterations (optional) Sets number of iterations for which to run bootstrap
 #' @param suppress.plot (optional) Suppresses the plot output
 #' @param return.values (optional) Exports the stored data for later access (e.g. slope and intercept calculated)
 #' @return description The resulting object contains the resulting intercept ($intercept), slope/beta ($beta), the slope in degrees ($slope.degrees)
@@ -18,6 +20,7 @@
 #'
 
 rotatogram <- function(x1,x2,x1.label="X1",x2.label="X2",
+                       error.bootstrap=FALSE,error.bootstrap.iterations = 500,
                        suppress.plot=FALSE,return.values=FALSE){
   # Analysis
   {
@@ -27,12 +30,22 @@ rotatogram <- function(x1,x2,x1.label="X1",x2.label="X2",
     beta <- sign(stats::cov(x1,x2))*stats::sd(x2)/stats::sd(x1)
     intercept <- mean(x2) - beta*mean(x1)
 
+    # Bootstrap
+    if (error.bootstrap == TRUE){
+      single.bootstrap <- function(){
+        df.bootstrap <- df[sample(1:nrow(df),size=nrow(df),replace=TRUE),]
+        beta <- sign(stats::cov(df.bootstrap$x1,df.bootstrap$x2))*stats::sd(df.bootstrap$x2)/stats::sd(df.bootstrap$x1)
+        intercept <- mean(df.bootstrap$x2) - beta*mean(df.bootstrap$x1)
+        return (data.frame(intercept,beta))
+      }
+      df.bootstrap.params <- do.call(rbind,replicate(error.bootstrap.iterations,single.bootstrap(),simplify=FALSE))
+    }
+
     x1.range <- (max(x1)-min(x1))
     x2.range <- (max(x2)-min(x2))
     coord.ratio <- x1.range/x2.range
 
-    # Find coordinates that are 10% longer
-
+    # Find x2 values at x1 min and max
     x2.x1min <- intercept + beta*min(x1)
     x2.x1max <- intercept + beta*max(x1)
 
@@ -41,38 +54,63 @@ rotatogram <- function(x1,x2,x1.label="X1",x2.label="X2",
 
   # Graphical output and extraction
   {
-    # Generate original chart
+    # Generate original chart and themes
     p.regular <- ggplot(data=df,aes(x=x1,y=x2))+
       theme_bw()+
       coord_fixed(ratio = coord.ratio) +
-      geom_point(alpha=0.5,shape=16,color="aquamarine3") +
-      geom_function(fun=Vectorize(function(x) {
-        return(beta*x+intercept)
-      }), color="darkslateblue",linetype="solid", linewidth=1.5)+
+      geom_point(alpha=0.5,shape=16,color="aquamarine3")+
       theme(
         panel.border = element_blank(),
         plot.background = element_blank(),
         panel.grid.minor = element_blank()
       )+
       xlab(x1.label)+
-      ylab(x2.label)+
+      ylab(x2.label)
+
+    # Add bootstrap lines
+    if (error.bootstrap == TRUE){
+      for (i in 1:nrow(df.bootstrap.params)){
+        p.regular <- p.regular +
+          geom_function(fun=Vectorize(function(x,i) {
+            return(df.bootstrap.params$beta[i]*x+df.bootstrap.params$intercept[i])
+          }), args = list(i = i),
+          color="darkslateblue", linewidth=.5,alpha=5*1/(nrow(df.bootstrap.params)))
+      }
+    }
+
+    # Add main lines
+    p.regular <- p.regular +
+      geom_function(fun=Vectorize(function(x) {
+        return(beta*x+intercept)
+      }), color="darkslateblue",linetype="solid", linewidth=1.5)+
       geom_hline(yintercept = min(x2)-(max(x2)-min(x2))*.05,color="darkslategrey",linetype=2)+
       geom_vline(xintercept = min(x1)-(max(x1)-min(x1))*.05,color="darkslategrey",linetype=2)
 
     # Change label angles
-    p.pre.rotated <- p.regular +
-      theme(
-        axis.text.x = element_text(angle = slope.degrees,vjust=.5),
-        axis.text.y = element_text(angle = slope.degrees,hjust=.5)
-      )
-
-    if (slope.degrees<=0){
-      p.pre.rotated <- p.pre.rotated +
+    if (beta>=0){
+      p.pre.rotated <- p.regular +
         theme(
-          axis.title.y = element_text(angle = 270),
-          axis.title.x = element_text(angle = 0)
+          axis.text.x = element_text(angle = slope.degrees,vjust=.5),
+          axis.text.y = element_text(angle = slope.degrees,hjust=.5),
+          axis.title.x = element_text(angle = 0),
+          axis.title.y = element_text(angle = 270)
+
+        )
+    } else {
+      p.pre.rotated <- p.regular +
+        theme(
+          axis.text.x = element_text(angle = slope.degrees+180,vjust=.5),
+          axis.text.y = element_text(angle = slope.degrees+180,hjust=.5),
+          axis.title.x = element_text(angle = 0),
+          axis.title.y = element_text(angle = 90)
         )
     }
+  }
+
+  if (beta>=0){
+    rotate.angle <- -slope.degrees
+  } else {
+    rotate.angle <- -slope.degrees-180
   }
 
   if (suppress.plot == FALSE){
@@ -80,9 +118,18 @@ rotatogram <- function(x1,x2,x1.label="X1",x2.label="X2",
     suppressWarnings(print(p.pre.rotated,
                            vp = grid::viewport(width = 0.75,
                                          height = 0.75,
-                                         angle = -slope.degrees)))
+                                         angle = rotate.angle)))
   }
-  output <- list("intercept"=intercept,"beta"=beta,"slope.degrees"=slope.degrees)
-  if (return.values==TRUE){return(output)}
+
+  if (return.values==TRUE){
+    output <- list("intercept"=intercept,"beta"=beta,"slope.degrees"=slope.degrees,
+                   "p.regular"=p.regular)
+    if (error.bootstrap == TRUE){
+      output <- c(output,
+                  list("intercept.SE" = stats::sd(df.bootstrap.params$intercept),
+                    "beta.SE" = stats::sd(df.bootstrap.params$beta)))
+    }
+    return(output)
+  }
 }
 
